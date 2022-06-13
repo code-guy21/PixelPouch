3d700675abd6454f390af2b2719bcdcc0c56be0e
Alexis San Javier <alexissj16@knights.ucf.edu>
2022-06-13 12:12:40 -0400

    added form in auth component for registering and logging in

--- client/src/components/Auth/index.js
+++ client/src/components/Auth/index.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, {useState} from 'react'
 import "./style.css"
 
 function Auth() {
+  const [toggle, setToggle] = useState(false)
+
+
   return (
-    <div className="auth">Auth</div>
+    <div className="auth">
+      <input placeholder={toggle ? "email" : "email or username"} className='form_input'/>
+      <input className={toggle ? "form_input": "hide form_input" } placeholder='username'/>
+      <input placeholder='password' className='form_input'/>
+      <button id="login" className='submit'>{ toggle ? "signup": "login"}</button>
+      <button onClick={() => setToggle(!toggle)}  id="signup" className='submit'>{ toggle ? "login": "signup"}</button>
+    </div>
   )
 }
 
