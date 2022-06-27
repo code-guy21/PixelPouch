3ca903b1ccd8ebe2474c98b07c302de9383a78e5
Alexis San Javier <alexissj16@knights.ucf.edu>
2022-06-27 08:29:11 -0400

    added functions for decoding JWT and for redirecting if unauthenticated using react-router-dom navigate hook

--- client/src/pages/Dashboard/index.js
+++ client/src/pages/Dashboard/index.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {useNavigate} from "react-router-dom"
 import Auth from "../../utils/auth"
 
 function Dashboard() {
+	const [loggedIn, setLoggedIn] = useState(false)
+	const navigate = useNavigate()
+
+	const logout = ()=>{
+		Auth.logout()
+		setLoggedIn(false)
+	}
+
+	useEffect( ()=> {
+		try {
+			let token = Auth.getUser()
+			console.log(token)
+			setLoggedIn(true)
+		} catch (error) {
+			navigate("/")
+		}
+	},[loggedIn])
+
 	return <div>
 		<h1>dashboard</h1>
-		<button onClick={Auth.logout}>logout</button>
+		<button onClick={logout}>logout</button>
 	</div>;
 }
 
--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -1,11 +1,21 @@
+import jwt_decode from "jwt-decode"
+
 class AuthService {
-  login (token) {
-        localStorage.setItem("auth_token", token)
-    }
-    
-    logout () {
-        localStorage.removeItem("auth_token")
-    }
+	login(token) {
+		localStorage.setItem('auth_token', token);
+	}
+
+	getUser(){
+		return jwt_decode(this.getToken())
+	}
+	
+	getToken(){
+		return localStorage.getItem("auth_token")
+	}
+
+	logout() {
+		localStorage.removeItem('auth_token');
+	}
 }
 
 export default new AuthService();
